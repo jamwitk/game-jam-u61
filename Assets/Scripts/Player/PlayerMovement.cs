29fabbae1092cd8186a30cd600b0c7928890f79b
Jamshid Abdukarimov <hushdilcss@gmail.com>
2023-04-08 02:12:04 +0300

    Start sahnesi düzenlendi, karakter hareketi ayarlandı.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public int jumpForce;
+        private Rigidbody2D _rb;
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.touchCount > 0 || Input.GetMouseButton(0) && transform.position.y < 4f)
+            {
+                _rb.AddForce(Vector2.up * jumpForce);
+            }
+        }
+    }
+}
