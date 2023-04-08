ff9f8ed1e50294376c559cf7938ed7b8fc396f05
GocerSongul <85079722+GocerSongul@users.noreply.github.com>
2023-04-08 19:27:17 +0300

    ultiBarsıfırlama

--- Assets/Scripts/Obstacles/BarConroller.cs
+++ Assets/Scripts/Obstacles/BarConroller.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 
 public class BarConroller : MonoBehaviour
 {
-    public float maxTime = 60f; 
-    public float fillPerSecond = 1f; 
+    public float maxTime = 40f; 
+    public float fillPerSecond = 2f; 
     
     void Update()
     {
         GameObject.Find("Bar").GetComponent<Image>().fillAmount = Amount.fillAmount;
         float deltaTime = Time.deltaTime; 
         Amount.fillAmount += (deltaTime / maxTime) * fillPerSecond; 
-        Amount.fillAmount = Mathf.Clamp01(Amount.fillAmount); 
+        Amount.fillAmount = Mathf.Clamp01(Amount.fillAmount);
+        if (Amount.fillAmount >= 1f)
+        {
+            Amount.fillAmount = 0f;
+        }
 
-        
     }
 }
